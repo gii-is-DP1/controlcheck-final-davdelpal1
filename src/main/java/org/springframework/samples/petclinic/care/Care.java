b093acefc8d6047dcf004ca6754141bcfa8d5c1e
David <davdelpal1@alum.us.es>
2022-01-21 10:33:22 +0100

    Test 1 funcionando

--- src/main/java/org/springframework/samples/petclinic/care/Care.java
+++ src/main/java/org/springframework/samples/petclinic/care/Care.java
@@ -2,15 +2,34 @@ package org.springframework.samples.petclinic.care;
 
 import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.pet.PetType;
 
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
-public class Care {
-    String name;
-    String description;
-    Set<PetType> compatiblePetTypes;
+@Table(name = "care")
+public class Care extends BaseEntity{
+	@NotEmpty
+	@Size(min = 5, max = 30)
+	@Column(name = "name", unique= true)
+    private String name;
+	@Column(name ="description")
+	@NotEmpty
+    private String description;
+	@ManyToMany(cascade = CascadeType.ALL)
+	@NotEmpty
+	@JoinColumn(name = "compatiblePetTypes")
+    private Set<PetType> compatiblePetTypes;
 }
